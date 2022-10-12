23dbdc38e66b50fb1e5d02949530ee8cee8425ad
SergeevSezar <sergeev.sezar@yandex.ru>
2022-10-13 06:57:00 +0900

    add view holder and adapter

--- app/src/main/java/rome/sezarion/officemanager/CrimeListFragment.kt
+++ app/src/main/java/rome/sezarion/officemanager/CrimeListFragment.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class CrimeListFragment : Fragment() {
 
@@ -32,6 +34,32 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         return view
+    }
+
+    private inner class CrimeHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+        val titleTextView : TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView : TextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    private inner class CrimeAdapter(val crimes : List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.fragment_crime_list, parent, false)
+            return CrimeHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
     }
 
     companion object {
