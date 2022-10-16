543fa346d01d1f7daacc4a848853fa34aa8be679
SergeevSezar <sergeev.sezar@yandex.ru>
2022-10-17 06:42:55 +0900

    add recycler view elements, holder logic and set clicklistener

--- app/src/main/java/rome/sezarion/officemanager/CrimeListFragment.kt
+++ app/src/main/java/rome/sezarion/officemanager/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +16,7 @@ import org.w3c.dom.Text
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView : RecyclerView
+    private var adapter : CrimeAdapter? = null
 
     private val crimeListViewModel : CrimeListViewModel by lazy {
         ViewModelProvider(this)[CrimeListViewModel::class.java]
@@ -33,28 +35,48 @@ class CrimeListFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        updateUI()
         return view
     }
 
-    private inner class CrimeHolder(view : View) : RecyclerView.ViewHolder(view) {
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
 
-        val titleTextView : TextView = itemView.findViewById(R.id.crime_title)
-        val dateTextView : TextView = itemView.findViewById(R.id.crime_date)
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        private lateinit var crime: Crime
+
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title_list_item)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date_list_item)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
+        override fun onClick(p0: View?) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     private inner class CrimeAdapter(val crimes : List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-            val view = layoutInflater.inflate(R.layout.fragment_crime_list, parent, false)
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
             return CrimeHolder(view)
         }
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+            holder.bind(crime)
         }
 
         override fun getItemCount(): Int {
