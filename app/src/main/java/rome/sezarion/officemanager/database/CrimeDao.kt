e7104881862a5143e25afa50d120ab0cf874198f
SergeevSezar <sergeev.sezar@yandex.ru>
2022-11-20 13:31:00 +0900

    upd live data use func

--- app/src/main/java/rome/sezarion/officemanager/database/CrimeDao.kt
+++ app/src/main/java/rome/sezarion/officemanager/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package rome.sezarion.officemanager.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import rome.sezarion.officemanager.Crime
@@ -8,8 +9,8 @@ import java.util.*
 @Dao
 interface CrimeDao {
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime
+    fun getCrime(id: UUID): LiveData<Crime>
 }
