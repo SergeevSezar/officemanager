d7b094656002f1fcfe69201bf5a8d4ff2f1c7ee2
SergeevSezar <sergeev.sezar@yandex.ru>
2022-11-22 00:20:07 +0900

    add crime detail view model add save logic in db

--- app/src/main/java/rome/sezarion/officemanager/MainActivity.kt
+++ app/src/main/java/rome/sezarion/officemanager/MainActivity.kt
@@ -25,5 +25,11 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
 
     override fun onCrimeSelected(crimeId: UUID) {
         Log.d(TAG, "MainActivity.onCrimeSelected: $crimeId")
+        val fragment = CrimeFragment.newInstance(crimeId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
