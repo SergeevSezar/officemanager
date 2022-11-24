85c1e5a96ba4eff42a4fe5a9d8f2ef7551f0915a
SergeevSezar <sergeev.sezar@yandex.ru>
2022-11-24 23:29:52 +0900

    add implicit intent

--- app/src/main/java/rome/sezarion/officemanager/database/CrimeDatabase.kt
+++ app/src/main/java/rome/sezarion/officemanager/database/CrimeDatabase.kt
@@ -3,12 +3,20 @@ package rome.sezarion.officemanager.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import rome.sezarion.officemanager.Crime
 
-@Database(entities = [Crime::class], version = 1)
+@Database(entities = [Crime::class], version = 2)
 @TypeConverters(CrimeTypeConverter::class)
 abstract class CrimeDatabase : RoomDatabase() {
 
     abstract fun CrimeDao(): CrimeDao
-
 }
+
+val migration_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Crime ADD COLUMN suspect TEXT NOT NULL DEFAULT ''"
+        )
+    } }
